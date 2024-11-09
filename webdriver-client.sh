4dfcb5c105a768c686d02396527d8eb8b71f5c98
gtsop <gtsop@protonmail.com>
2024-11-10 01:01:47 +0200

    feature: add status and timeout functions

--- webdriver-client.sh
+++ webdriver-client.sh
@@ -12,6 +12,7 @@ new_session() {
 
 # 8.3 Delete Session
 #
+
 delete_session() {
     endpoint_url=$1;
     session_id=$2
@@ -19,8 +20,42 @@ delete_session() {
     wget -q -O - --method=delete "$endpoint_url"/session/"$session_id"
 }
 
+# 8.4 Status
+#
+status() {
+    endpoint_url=$1;
+
+    wget -q -O - "$endpoint_url"/status
+}
+
+# 9.1 Get Timeouts
+#
+get_timeouts() {
+    endpoint_url=$1;
+    session_id=$2
+
+    wget -q -O - "$endpoint_url"/session/"$session_id"/timeouts
+}
+
+# 9.2 Set Timeouts
+#
+set_timeouts() {
+    endpoint_url=$1;
+    session_id=$2
+    payload=$3
+
+    wget -q -O - --header="Content-Type: application/json" --post-data="$payload" "$endpoint_url"/session/"$session_id"/timeouts
+}
+
+status http://localhost:4444
+
 response=$(new_session http://localhost:4444 '{"capabilities": { "browserName": "icecat" } }')
 
 session_id=$(echo "$response" | sed 's/.*"sessionId":"\(.[^"]*\)".*/\1/g')
 
+set_timeouts http://localhost:4444 "$session_id" '{"script":1, "pageLoad":2, "implicit":3}'
+
+get_timeouts http://localhost:4444 "$session_id"
+
 delete_session http://localhost:4444 "$session_id"
+
